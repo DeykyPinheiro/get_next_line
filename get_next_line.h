ca79310254d3564c7bdbc15cca074ae6beed1382
deykypinheiro <pinheiromikael96@gmail.com>
2021-08-21 01:34:55 -0300

    header added to all files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: demikael <pinheiromikael96@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 15:00:54 by demikael          #+#    #+#             */
+/*   Updated: 2021/08/20 16:34:37 by demikael         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "get_next_line.h"
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
